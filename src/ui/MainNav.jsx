a632258737edec2a5e670a6f721962a642f101d8
reyhanehsarkhani <reyhanesarkhani@gmail.com>
2026-04-07 16:38:57 +0200

    add main navigation

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,12 +1,19 @@
+import { NavLink } from "react-router";
 import styled from "styled-components";
-
+import {
+  HiOutlineHome,
+  HiCalendarDays,
+  HiHomeModern,
+  HiUsers,
+  HiCog6Tooth,
+} from "react-icons/hi2";
 const NavList = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 0.8rem;
 `;
 
-const Link = styled.a`
+const StyledNavLink = styled(NavLink)`
   &:link,
   &:visited {
     display: flex;
@@ -44,3 +51,43 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+function MainNav() {
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <StyledNavLink to="/dashboard">
+            <HiOutlineHome />
+            <span>Home</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/bookings">
+            <HiCalendarDays />
+            <span>Bookings</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/cabins">
+            <HiHomeModern />
+            <span>Cabins</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/users">
+            <HiUsers />
+            <span>users</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/settings">
+            <HiCog6Tooth />
+            <span>settings</span>
+          </StyledNavLink>
+        </li>
+      </NavList>
+    </nav>
+  );
+}
+
+export default MainNav;
