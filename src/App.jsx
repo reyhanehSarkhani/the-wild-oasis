a779cbfc7efe6df385ba72f958ef05e507e71446
reyhanehsarkhani <reyhanesarkhani@gmail.com>
2026-04-07 14:40:21 +0200

    setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import GlobalStyles from "./styles/GlobalStyles";
-
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Login from "./pages/Login";
+import Account from "./pages/Account";
+import Settings from "./pages/Settings";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div>the wild oasis</div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
