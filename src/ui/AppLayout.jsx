5b12c5ed5689c97fb932f546d41929797e34f922
reyhanehsarkhani <reyhanesarkhani@gmail.com>
2026-04-07 15:55:19 +0200

    build the app layout

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,27 @@
+import { Outlet } from "react-router";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import styled from "styled-components";
+const StyledLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+function AppLayout() {
+  return (
+    <StyledLayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledLayout>
+  );
+}
+
+export default AppLayout;
