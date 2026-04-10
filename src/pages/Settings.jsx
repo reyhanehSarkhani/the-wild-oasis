b45aca065b890d9c7a08c3bac6dd241881919a17
reyhanehsarkhani <reyhanesarkhani@gmail.com>
2026-04-10 12:10:16 +0200

    fetching and updating application setting

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,13 @@
 import Heading from "../ui/Heading";
-
+import Row from "../ui/Row";
+import UpdateSettings from "../features/settings/UpdateSettingsForm";
 function Settings() {
-  return <Heading as="h1">Update hotel settings</Heading>;
+  return (
+    <Row>
+      <Heading>Update hotel settings</Heading>
+      <UpdateSettings />
+    </Row>
+  );
 }
 
 export default Settings;
