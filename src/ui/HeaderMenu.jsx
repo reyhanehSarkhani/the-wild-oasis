7b4c64d9d765c0e777d66c1c7b7a7edf0f76f2f8
reyhanehsarkhani <reyhanesarkhani@gmail.com>
2026-04-23 11:27:41 +0200

    build the app header

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import Logout from "../features/authentication/Logout";
+import { HiOutlineUser } from "react-icons/hi2";
+import ButtonIcon from "./ButtonIcon";
+import { useNavigate } from "react-router";
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+`;
+
+function HeaderMenu() {
+  const navigate = useNavigate();
+
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
+
+export default HeaderMenu;
