02981cdfe0952f968baf223a18f91c2b0b80c372
reyhanehsarkhani <reyhanesarkhani@gmail.com>
2026-04-23 08:50:20 +0200

    fix a small bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -9,8 +9,8 @@ export function useLogin() {
 
   const { mutate: login, isPending } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: (user) => {
-      queryCLient.setQueriesData(["user"], user);
+    onSuccess: (data) => {
+      queryCLient.setQueryData(["user"], data.user);
       navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
