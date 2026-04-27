494bc8b507603a283ad3e804709ccaad5576a70a
reyhanehsarkhani <reyhanesarkhani@gmail.com>
2026-04-27 18:19:35 +0200

    fix a bug

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,7 +2,7 @@ import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import AddCabin from "../features/cabins/AddCabin";
-import CabinTableOperations from "../features/Cabins/cabinTableOperations";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 function Cabins() {
   return (
     <>
