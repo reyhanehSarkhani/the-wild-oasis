9292e65b8dd28cd6952d64a406003d68eb953e75
reyhanehsarkhani <reyhanesarkhani@gmail.com>
2026-04-27 16:13:35 +0200

    fix a bug in app layout UI

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -11,7 +11,7 @@ const StyledLayout = styled.div`
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
-  overflow: scroll;
+  overflow-y: auto;
 `;
 const Container = styled.div`
   max-width: 120rem;
